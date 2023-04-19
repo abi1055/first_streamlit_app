fc3656fd4168f5df4bc559fb84974ef37598c4ce
abi1055 <121970001+abi1055@users.noreply.github.com>
2023-04-19 17:14:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import numpy 
 
-fruits = pandas.csv_read('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+fruits = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(fruits)
 
 streamlit.title('My Parents New Healthy Diner Menu')
