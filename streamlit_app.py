08fadc79cb3113890d1f695949f8c93ae7486d56
abi1055 <121970001+abi1055@users.noreply.github.com>
2023-04-20 09:36:00 -0400

    Update streamlit_app.py
    adding in the fruit smoothie ingredients/menu at the bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@ import streamlit
 import pandas
 import numpy 
 
-fruits = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(fruits)
-
 streamlit.title('My Parents New Healthy Diner Menu')
 streamlit.header('🍞 Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -13,4 +10,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-  
+fruits = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(fruits)
